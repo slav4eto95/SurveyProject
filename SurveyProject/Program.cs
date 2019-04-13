e8898ef9fae0d05d4ea15f24c0aeb860193f6395
Slavena Koleva <slavena_kolevaa@abv.bg>
2019-04-13 14:00:00 +0300

    Add project files.

---
+++ SurveyProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SurveyProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
