9a243e574753838d0b0968a2ca3ca8af6e9c9a66
Slavena Koleva <slavena_kolevaa@abv.bg>
2019-04-13 15:22:29 +0300

    Added a class diagram for the project

--- SurveyProject/Program.cs
+++ SurveyProject/Program.cs
@@ -7,16 +7,16 @@ namespace SurveyProject
         private static void Main(string[] args)
         {
             Survey survey = new Survey("My Survey");
+
             TextQuestion question1 = new TextQuestion { Label = "Where are you from?" };
             TextQuestion question2 = new TextQuestion { Label = "Where do you live?" };
 
-            var Score = survey.GetScore();
+            survey.AddQuestion(question1);
+            survey.AddQuestion(question2);
 
-            /*survey.AddQuestion(question1);
-            survey.AddQuestion(question2);*/
-
-            Console.WriteLine(survey.Title);
-            Console.WriteLine($"Your score: {Score}");
+            int surveyScore = survey.GetScore();
+            
+            Console.WriteLine($"Your score: {surveyScore}");
         }
     }
 }
